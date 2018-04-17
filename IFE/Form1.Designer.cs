4c3a3e5bb6afa8ddba08361357b9a6f9bd9ff167
Дмитрий Аблаев <dablaev@yandex.ru>
2018-04-18 00:42:59 +0700

    Добавьте файлы проекта.

---
+++ IFE/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace IFE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(517, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "10";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(650, 206);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 28);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.dataGridView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView2);
+            this.splitContainer1.Size = new System.Drawing.Size(650, 482);
+            this.splitContainer1.SplitterDistance = 206;
+            this.splitContainer1.TabIndex = 6;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView2.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(650, 272);
+            this.dataGridView2.TabIndex = 6;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3,
+            this.toolStripButton4});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(650, 25);
+            this.toolStrip1.TabIndex = 7;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(99, 22);
+            this.toolStripButton1.Text = "Подключить БД";
+            this.toolStripButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.Overlay;
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(150, 22);
+            this.toolStripButton2.Text = "Импортировать .xls файл";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(140, 22);
+            this.toolStripButton3.Text = "Сохранить данные в БД";
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(120, 22);
+            this.toolStripButton4.Text = "Проверка значений";
+            this.toolStripButton4.Click += new System.EventHandler(this.toolStripButton4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 510);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+    }
+}
+
---
+++ IFE/Form1.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using System.Windows.Forms;
+
+namespace IFE
+{
+
+    public partial class Form1 : Form
+    {
+        string cons = "", conStr = "";
+        static string selectCommand = "SELECT * FROM [dbo].[standarts]"; 
+        static string selectCommand1 = "SELECT * FROM [dbo].[import]";
+        string insertCommand = "";
+        SqlConnection cn = null;
+        SqlDataAdapter dataAdapter = null;
+        SqlDataAdapter dataAdapter1 = null;
+        SqlCommand com1 = null;
+        SqlCommandBuilder bldr = null;
+        SqlCommandBuilder bldr1 = null;
+        System.Data.DataTable dataTable = null;
+        System.Data.DataTable dataTable1 = null;
+        int counter = 3, rowCounter = 1;
+
+
+
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            //Подключение к БД
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+
+                cons = openFileDialog1.FileName;
+
+                conStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + cons + "";
+
+
+                cn = new SqlConnection(); //Подключение SQL
+                //cn.ConnectionString = conStr;
+
+                using (cn)
+                {
+
+                    cn.ConnectionString = conStr; //Строка подключения к бд
+                    try
+                    {
+                        dataAdapter = new SqlDataAdapter(); //Инициализация адаптера для источника данных
+                        bldr = new SqlCommandBuilder(dataAdapter); //Инициализация построителя команд SQL, основываясь на подключенном адаптере
+
+                        SqlCommand sqlCommand = new SqlCommand(selectCommand, cn);//Инициализация команды SELECT с использования подключения SQL
+                        dataAdapter.SelectCommand = sqlCommand; //Присвоение адаптеру готовой Select команды с подключением 
+                        dataAdapter.InsertCommand = bldr.GetInsertCommand(); //Автогенерация команды insert(создание новой строки БД) и присвоение адаптеру
+                        dataAdapter.UpdateCommand = bldr.GetUpdateCommand(); //Автогенерация команды update(обновление существующей строки) и присвоение адаптеру
+
+                        cn.Open(); //Открытие подключения
+
+                        dataTable = new System.Data.DataTable(); //Инициализируем табличное представление
+                        dataAdapter.Fill(dataTable); //Заполняем табличное представление значениями из адаптера
+                        dataGridView1.DataSource = dataTable; //Присваиваем табличное представление(таблицу) как источник данных для dataGridView1
+                        dataGridView1.Columns[0].Visible = false;
+
+                        //Заполняется верхний dataGridView1 из таблицы standarts
+
+                    }
+                    catch (SqlException ex)
+                    {
+                        MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK);
+                    }
+                    finally
+                    {
+                        cn.Close();//Закрытие подключения
+                    }
+
+                }
+
+                using (cn)
+                {
+                    //Все по аналогии с dataGridView1
+                    cn.ConnectionString = conStr;
+                    try
+                    {
+                        cn.Open();
+                        dataAdapter1 = new SqlDataAdapter();
+                        bldr1 = new SqlCommandBuilder(dataAdapter1);
+
+                        SqlCommand sqlCommand = new SqlCommand(selectCommand1, cn);
+                        dataAdapter1.SelectCommand = sqlCommand;
+                        dataAdapter1.InsertCommand = bldr1.GetInsertCommand();
+                        dataAdapter1.UpdateCommand = bldr1.GetUpdateCommand();
+
+
+
+                        dataTable1 = new System.Data.DataTable();
+                        dataAdapter1.Fill(dataTable1);
+                        dataGridView2.DataSource = dataTable1;
+                        dataGridView2.Columns[0].Visible = false;
+
+                        //По тому же принципу заполняется нижний dataGridView2 из таблицы import
+
+
+                    }
+                    catch (SqlException ex)
+                    {
+                        MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK);
+                    }
+                    finally
+                    {
+                        cn.Close();
+                    }
+
+                }
+            }
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            //Считывание данных из .xls
+            counter = 3;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) //Диалоговое окно, которое срабатывает при выборе файла и нажатии ок
+            {
+                Microsoft.Office.Interop.Excel.Application ObjExcel = new Microsoft.Office.Interop.Excel.Application(); //Открывает экземпляр Excel
+
+                Microsoft.Office.Interop.Excel.Workbook ObjWorkBook = ObjExcel.Workbooks.Open(openFileDialog1.FileName, 0, false, 5, "", "", false, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "", true, false, 0, true, false, false);//Открытие файла с книгой
+
+                Microsoft.Office.Interop.Excel.Worksheet ObjWorkSheet;// Инициализация  страницы
+                ObjWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)ObjWorkBook.Sheets[2];// Выбор конкретной лист
+
+                for (int i = 1; i < 3; i++)
+                {
+
+                    Microsoft.Office.Interop.Excel.Range range = ObjWorkSheet.get_Range("C" + Convert.ToString(i + 1), "C" + Convert.ToString(i + 1)); //Считывание данных с конкретной ячейки листа
+
+                    dataGridView1.Rows[0].Cells[i].Value = range.Text;// Записываем полученное значение ячейки в dataGridView1
+
+                }
+
+                for (int i = 1; i < 11; i++)
+                {
+
+                    if (i == 3 || i == 5 || i == 7 || i == 8 || i == 9)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        Microsoft.Office.Interop.Excel.Range range = ObjWorkSheet.get_Range("F" + Convert.ToString(i + 10), "F" + Convert.ToString(i + 10));
+                        dataGridView1.Rows[0].Cells[counter].Value = range.Text;
+                        counter++;
+                    }
+
+                }
+
+                dataGridView2.Rows[0].Cells[0].Value = 1;
+
+                int sheetsCount = ObjWorkBook.Sheets.Count;
+
+                for (int j = 3; j < (Convert.ToInt32(textBox1.Text)+3); j++)
+                {
+                    counter = 4;
+
+                    ObjWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)ObjWorkBook.Sheets[j]; //Выбор листов в цикле
+
+
+                    for (int i = 1; i < 4; i++)
+                    {
+
+                        Microsoft.Office.Interop.Excel.Range range = ObjWorkSheet.get_Range("C" + Convert.ToString(i + 1), "C" + Convert.ToString(i + 1));
+
+                        dataGridView2.Rows[rowCounter - 1].Cells[i].Value = range.Text;
+
+                    }
+
+                    for (int i = 1; i < 11; i++)
+                    {
+
+                        if (i == 2 || i == 4 || i == 6 || i == 8 || i == 9)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            if (i == 5)
+                            {
+                                Microsoft.Office.Interop.Excel.Range range = ObjWorkSheet.get_Range("H" + Convert.ToString(i + 10), "H" + Convert.ToString(i + 10));
+                                dataGridView2.Rows[rowCounter - 1].Cells[counter].Value = range.Text;
+                                counter++;
+                            }
+                            else
+                            {
+                                Microsoft.Office.Interop.Excel.Range range = ObjWorkSheet.get_Range("G" + Convert.ToString(i + 10), "G" + Convert.ToString(i + 10));
+                                if (range.Text != "-")
+                                {
+                                    dataGridView2.Rows[rowCounter - 1].Cells[counter].Value = range.Text;
+                                    counter++;
+                                }
+                                else
+                                {
+                                    dataGridView2.Rows[rowCounter - 1].Cells[counter].Value = 0;
+                                }
+                            }
+                        }
+
+                        
+
+                    }
+                    SqlCommand com1 = new SqlCommand(); //Инициализация экземпляра команды SQL
+                    using (SqlConnection cn = new SqlConnection())// Инициализация экземпляра подключения
+                    {
+                        cn.ConnectionString = conStr;//Приствоение экземпяляру подключения соответствующей строки подключения
+                        cn.Open();//Открытие подключения
+                        com1.CommandText = String.Format("INSERT INTO [dbo].[import]([Название], [Партия], [Дата испытаний], [Вес, результат], [Емкость, Ф, результат], [ESR, мОм, результат], [КПД, %, результат], [Температура, С, результат] ) " +
+                            "VALUES ( '{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}')", dataGridView2.Rows[rowCounter].Cells[1].Value, dataGridView2.Rows[rowCounter].Cells[2].Value, dataGridView2.Rows[rowCounter].Cells[3].Value, 
+                            dataGridView2.Rows[rowCounter].Cells[4].Value, dataGridView2.Rows[rowCounter].Cells[5].Value, 
+                            dataGridView2.Rows[rowCounter].Cells[6].Value, dataGridView2.Rows[rowCounter].Cells[7].Value, 
+                            dataGridView2.Rows[rowCounter].Cells[8].Value);
+                        //Использование не сохраненных данных, записанных в dataGridview2 на предыдущем шаге для создания строки ввода(INSERT)
+                        //
+                        com1.Connection = cn;
+                        com1.ExecuteNonQuery();
+
+                        cn.Close();
+                    }
+
+                    rowCounter++;
+                    //counter = 3;
+
+
+
+                }
+                ObjExcel.Quit();// Закрыть открытый экземпляр Excel
+            }
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+
+            using (SqlConnection cn = new SqlConnection(conStr)) //инициализация подключения SQL с присвоением строки подключения к БД
+            {
+                SqlCommand sqlCom = new SqlCommand(selectCommand, cn); //Инициализация команды SELECT sql для таблицы standarts
+                SqlCommand sqlCom1 = new SqlCommand(selectCommand1, cn); //Инициализация команды SELECT sql для таблицы import
+
+                dataAdapter = new SqlDataAdapter(selectCommand, cn); //Переопределяется адаптер для таблицы standarts
+                dataAdapter1 = new SqlDataAdapter(selectCommand1, cn); //Переопределяется адаптер для таблицы import
+
+                SqlCommandBuilder builder = new SqlCommandBuilder(dataAdapter); //Инициализация построителя команд для адаптера таблицы standarts
+                SqlCommandBuilder builder1 = new SqlCommandBuilder(dataAdapter1); //Инициализация построителя команд для адаптера таблицы import
+
+                dataAdapter.SelectCommand = sqlCom; //Присвоение ранее созданной команды Select адаптеру
+                dataAdapter.InsertCommand = builder.GetInsertCommand(); //Присвоение сгенерированной команды INSERT адаптеру таблицы standarts
+                dataAdapter.UpdateCommand = builder.GetUpdateCommand(); // Присвоение сгенерированной команды UPDATE адаптеру таблицы standarts
+
+                dataAdapter1.SelectCommand = sqlCom1; //Присвоение ранее созданной команды Select адаптеру
+                dataAdapter.InsertCommand = builder1.GetInsertCommand(); //Присвоение сгенерированной команды INSERT адаптеру таблицы import
+                dataAdapter.UpdateCommand = builder.GetUpdateCommand(); // Присвоение сгенерированной команды UPDATE адаптеру таблицы import
+
+                dataAdapter.Update(dataTable); //Вызывает соответствующие инструкции INSERT, UPDATE или DELETE для каждой вставки, обновления или удаления строки в указанном DataTable standarts
+                dataAdapter1.Update(dataTable1); //Вызывает соответствующие инструкции INSERT, UPDATE или DELETE для каждой вставки, обновления или удаления строки в указанном DataTable import
+                cn.Close(); //закрытие подключения
+
+            }
+        }
+
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; dataGridView1.Rows[0].Cells[1].Value.ToString() == dataGridView2.Rows[i].Cells[1].Value.ToString(); i++)
+            {
+               
+                if (Convert.ToInt32(dataGridView2.Rows[i].Cells[4].Value) < Convert.ToInt32(dataGridView1.Rows[0].Cells[3].Value))
+                    dataGridView2.Rows[i].Cells[4].Style.BackColor = System.Drawing.Color.Green;
+                else
+                    dataGridView2.Rows[i].Cells[4].Style.BackColor = System.Drawing.Color.Red;
+
+                if (Convert.ToInt32(dataGridView2.Rows[i].Cells[5].Value) == Convert.ToInt32(dataGridView1.Rows[0].Cells[4].Value))
+                    dataGridView2.Rows[i].Cells[5].Style.BackColor = System.Drawing.Color.Green;
+                else
+                    dataGridView2.Rows[i].Cells[5].Style.BackColor = System.Drawing.Color.Red;
+
+                if (Convert.ToInt32(dataGridView2.Rows[i].Cells[6].Value) < Convert.ToInt32(dataGridView1.Rows[0].Cells[5].Value))
+                    dataGridView2.Rows[i].Cells[6].Style.BackColor = System.Drawing.Color.Green;
+                else
+                    dataGridView2.Rows[i].Cells[6].Style.BackColor = System.Drawing.Color.Red;
+
+                if (Convert.ToInt32(dataGridView2.Rows[i].Cells[7].Value) > Convert.ToInt32(dataGridView1.Rows[0].Cells[6].Value))
+                    dataGridView2.Rows[i].Cells[7].Style.BackColor = System.Drawing.Color.Green;
+                else
+                    dataGridView2.Rows[i].Cells[7].Style.BackColor = System.Drawing.Color.Red;
+
+                if (Convert.ToInt32(dataGridView2.Rows[i].Cells[8].Value) < Convert.ToInt32(dataGridView1.Rows[0].Cells[7].Value))
+                    dataGridView2.Rows[i].Cells[8].Style.BackColor = System.Drawing.Color.Green;
+                else
+                    dataGridView2.Rows[i].Cells[8].Style.BackColor = System.Drawing.Color.Red;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+    }
+}
